0a402c7a440ff39042e67c8149871faca1216a17
Jean Michel Rouly <michel.rouly@gmail.com>
2017-07-05 23:41:30 -0400

    Send java format changes back to files.

--- pre_commit_hooks/google-java-format.sh
+++ pre_commit_hooks/google-java-format.sh
@@ -4,4 +4,4 @@ DIR=$( cd $( dirname "${BASH_SOURCE[0]}") && pwd )
 
 FILES=${@:1}
 
-java -jar "$DIR/google-java-format/google-java-format-1.3-all-deps.jar" $FILES
+java -jar "$DIR/google-java-format/google-java-format-1.3-all-deps.jar" -i $FILES
